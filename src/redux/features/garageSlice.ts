32196867421e9a8365e627ef51cc500a702b34ff
GagoYaxsh <gagikmartirosyan753@gmail.com>
2025-08-17 19:31:46 +0400

    Feat: implement car removal functionality on mock data

--- src/redux/features/garageSlice.ts
+++ src/redux/features/garageSlice.ts
@@ -23,11 +23,17 @@ const garageSlice = createSlice({
   name: 'garage',
   initialState,
   reducers: {
+    setCars(state, action: PayloadAction<Car[]>) {
+      state.cars = action.payload;
+    },
     addCar(state, action: PayloadAction<Car>) {
       state.cars.push(action.payload);
+    },
+    removeCar(state, action: PayloadAction<number>) {
+      state.cars = state.cars.filter((car) => car.id !== action.payload);
     },
   },
 });
 
-export const { addCar } = garageSlice.actions;
+export const { setCars, addCar, removeCar } = garageSlice.actions;
 export default garageSlice.reducer;
