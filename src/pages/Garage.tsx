56be6c72802904e455e5bd6d3366a42431a7e196
GagoYaxsh <gagikmartirosyan753@gmail.com>
2025-08-17 19:31:07 +0400

    Style: update Garage page styling

--- src/pages/Garage.tsx
+++ src/pages/Garage.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { addCar } from '../redux/features/garageSlice';
+import { addCar, removeCar } from '../redux/features/garageSlice';
 import { useAppDispatch, useAppSelector } from '../redux/hooks';
 import { Link } from 'react-router-dom';
 import carSvgs from '../assets/carSvgs';
@@ -19,6 +19,10 @@ export default function Garage() {
     const newCar: Car = { id: Date.now(), name, color };
     dispatch(addCar(newCar));
     setName('');
+  };
+
+  const handleRemoveCar = (id: number) => {
+    dispatch(removeCar(id));
   };
 
   return (
@@ -78,17 +82,36 @@ export default function Garage() {
       <div className="w-300 m-auto h-15 bg-[length:60px] bg-[url('/border.png')] bg-repeat-x bg-center"></div>
 
       <div>
-        <ul>
-          {cars.map((car, i) => {
-            const Car = carSvgs[i % 9];
-            return (
-              <li key={car.id} className="flex items-center ">
-                <p className="w-30 mr-4">{car.name}</p>
-                <Car className="w-70 h-35" fill={car.color} />
-              </li>
-            );
-          })}
-        </ul>
+        {cars.map((car, i) => {
+          const Car = carSvgs[i % 9];
+          return (
+            <div key={car.id} className="flex items-center border-b-white border-b-4">
+              <div className="flex items-center gap-4">
+                <div className="flex flex-col gap-3">
+                  <button className="px-3 font-semibold rounded-2xl bg-yellow-400 text-black">
+                    SELECT
+                  </button>
+                  <button
+                    onClick={() => handleRemoveCar(car.id)}
+                    className="px-3 font-semibold rounded-2xl bg-pink-700 text-black"
+                  >
+                    REMOVE
+                  </button>
+                </div>
+                <div className="flex flex-col gap-3">
+                  <button className="px-3 font-semibold rounded-2xl bg-green-700 text-black">
+                    START
+                  </button>
+                  <button className="px-3 font-semibold rounded-2xl bg-red-700 text-black">
+                    STOP
+                  </button>
+                </div>
+              </div>
+              <Car className="w-40 h-20" fill={car.color} />
+              <p className="ml-2">{car.name}</p>
+            </div>
+          );
+        })}
       </div>
       <div className="w-300 m-auto h-15 bg-[length:60px] bg-[url('/border.png')] bg-repeat-x bg-center"></div>
     </>
